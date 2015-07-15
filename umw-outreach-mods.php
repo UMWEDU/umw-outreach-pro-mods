5d4fb33a90f43784770ddd648811ad591ec6b321
Curtiss Grymala <curt_grymala@yahoo.com>
2015-07-15 17:21:24 -0400

    Change from unused WP_MANAGE_GLOBAL_HEADER_FOOTER constant to UMW_IS_ROOT constant Add some additional details and potential caching to custom feed functionality

--- umw-outreach-mods.php
+++ umw-outreach-mods.php
@@ -9,11 +9,9 @@
 if ( ! class_exists( 'UMW_Outreach_Mods' ) ) {
 	require_once( plugin_dir_path( __FILE__ ) . '/classes/class-umw-outreach-mods.php' );
 	
-	define( 'WP_MANAGE_GLOBAL_HEADER_FOOTER', true );
-	
 	function inst_umw_outreach_mods_obj() {
 		global $umw_outreach_mods_obj, $blog_id;
-		if ( defined( 'WP_MANAGE_GLOBAL_HEADER_FOOTER' ) && WP_MANAGE_GLOBAL_HEADER_FOOTER && 1 === absint( $blog_id ) ) {
+		if ( defined( 'UMW_IS_ROOT' ) && is_numeric( UMW_IS_ROOT ) && absint( UMW_IS_ROOT ) === absint( $blog_id ) ) {
 			$umw_outreach_mods_obj = new UMW_Outreach_Mods;
 		} else {
 			$umw_outreach_mods_obj = new UMW_Outreach_Mods_Sub;
