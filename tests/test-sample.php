c0f7b68ed94aa008321a422b335edd024a7b16fe
Curtiss Grymala <cgrymala@umw.edu>
2018-04-13 11:44:11 -0400

    Test a different process of integrating CircleCI

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Umw_Outreach_Mods
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
