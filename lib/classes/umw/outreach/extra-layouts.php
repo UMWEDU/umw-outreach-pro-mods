076de3a51d1069e17eb98fc919a8f5c8c4c903a6
Curtiss Grymala <cgrymala@umw.edu>
2021-04-16 10:17:38 -0400

    Attempt to test enabling the primary sidebar in older layouts

--- lib/classes/umw/outreach/extra-layouts.php
+++ lib/classes/umw/outreach/extra-layouts.php
@@ -14,7 +14,18 @@ if ( ! class_exists( 'Extra_Layouts' ) ) {
 				return;
 			}
 
-			if( defined( 'UMW_OUTREACH_ENABLE_SIDEBAR') ) {
+			add_action( 'after_setup_theme', array( $this, 'reenable_sidebars' ), 12 );
+		}
+
+		/**
+		 * Re-enable the primary sidebar after it's been disabled by the base class
+		 *
+		 * @access public
+		 * @return void
+		 * @since  2021.04
+		 */
+		public function reenable_sidebars() {
+			if ( defined( 'UMW_OUTREACH_ENABLE_SIDEBAR' ) ) {
 				add_action( 'genesis_sidebar', 'genesis_do_sidebar', 11 );
 			}
 		}
