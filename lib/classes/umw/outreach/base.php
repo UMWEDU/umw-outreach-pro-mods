5e5bde33742937c693736410defd6f2c0250a44a
Curtiss Grymala <cgrymala@umw.edu>
2021-11-02 11:59:50 -0400

    Change Home page CTA styles

--- lib/classes/umw/outreach/base.php
+++ lib/classes/umw/outreach/base.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'Base' ) ) {
 		/**
 		 * @var string $version holds the version number that's appended to script/style files
 		 */
-		var $version = '2021.11.02.09';
+		var $version = '2021.11.02.10';
 		/**
 		 * @var null|string $header_feed holds the URL of the custom header feed
 		 */
