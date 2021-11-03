02aa0eb8e1799f974102ee5bc32fa20e424e8399
Curtiss Grymala <cgrymala@umw.edu>
2021-11-03 17:27:48 -0400

    Begin building home page facts widget

--- lib/classes/umw/outreach/base.php
+++ lib/classes/umw/outreach/base.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'Base' ) ) {
 		/**
 		 * @var string $version holds the version number that's appended to script/style files
 		 */
-		var $version = '2021.11.03.03';
+		var $version = '2021.11.03.04';
 		/**
 		 * @var null|string $header_feed holds the URL of the custom header feed
 		 */
