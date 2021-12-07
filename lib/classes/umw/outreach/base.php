64d4767aacc0459f5d52c5eaacb11d8b090b5b6a
Curtiss Grymala <cgrymala@umw.edu>
2021-12-07 09:35:38 -0500

    Bump version number

--- lib/classes/umw/outreach/base.php
+++ lib/classes/umw/outreach/base.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'Base' ) ) {
 		/**
 		 * @var string $version holds the version number that's appended to script/style files
 		 */
-		var $version = '2021.11.08.04';
+		var $version = '2021.12.07.01';
 		/**
 		 * @var null|string $header_feed holds the URL of the custom header feed
 		 */
