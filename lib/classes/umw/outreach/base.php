16bfcfd96550b3808e486593b1ad070140a435f3
Curtiss Grymala <cgrymala@umw.edu>
2022-01-31 14:57:17 -0500

    Update colors for CTA links

--- lib/classes/umw/outreach/base.php
+++ lib/classes/umw/outreach/base.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'Base' ) ) {
 		/**
 		 * @var string $version holds the version number that's appended to script/style files
 		 */
-		var $version = '2022.01.31.01';
+		var $version = '2022.01.31.02';
 		/**
 		 * @var null|string $header_feed holds the URL of the custom header feed
 		 */
