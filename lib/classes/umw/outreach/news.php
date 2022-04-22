3cee0bec33e4df82fad38f7f30f67d2b27ede6b2
Curtiss Grymala <cgrymala@umw.edu>
2022-04-22 16:33:47 -0400

    Attempt to fix FTS issues

--- lib/classes/umw/outreach/news.php
+++ lib/classes/umw/outreach/news.php
@@ -635,6 +635,10 @@ if ( ! class_exists( 'News' ) ) {
 			$instagram_name  = $this->get_instagram_id( $instagram_name );
 			$instagram_token = $this->get_instagram_token( $instagram_name );
 
+            if ( isset( $this->fts_data_protection ) ) {
+                $instagram_token = $this->fts_data_protection->decrypt( $instagram_token );
+            }
+
 			do_shortcode( sprintf( '[fts_instagram instagram_id=%1$s access_token=%3$s pics_count=%4$s type=basic super_gallery=yes columns=3 force_columns=no space_between_photos=1px icon_size=65px hide_date_likes_comments=no]', $instagram_name, $this->latest_social_posts_count, $instagram_token ) );
 
 			$posts = get_transient( sprintf( 'fts_t_instagram_basic_cache%1$d_num%2$d', $instagram_name, $this->latest_social_posts_count ) );
