e0354f10bc7a0f757642823aa54ca34c01a6ad95
Curtiss Grymala <cgrymala@umw.edu>
2023-01-31 08:36:56 -0500

    Attempt to allow "Featured" to be edited in Quick Edit and Bulk Edit

--- lib/classes/umw/outreach/admin/columns.php
+++ lib/classes/umw/outreach/admin/columns.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace UMW\Outreach\Admin;
 
 if ( ! class_exists( 'Columns' ) ) {
@@ -12,12 +13,12 @@ if ( ! class_exists( 'Columns' ) ) {
 		 * @var bool $is_news
 		 * @access private
 		 */
-		private bool $is_news=false;
+		private bool $is_news = false;
 		/**
 		 * @var bool $is_events
 		 * @access private
 		 */
-		private bool $is_events=false;
+		private bool $is_events = false;
 
 		/**
 		 * Construct our Columns object
@@ -49,12 +50,12 @@ if ( ! class_exists( 'Columns' ) ) {
 		 * Instantiate necessary actions
 		 *
 		 * @access public
-		 * @since  2023.01
 		 * @return void
+		 * @since  2023.01
 		 */
 		public function init() {
 			$this->is_events = ( defined( 'UMW_LOCALIST_VERSION' ) );
-			$this->is_news = is_a( $GLOBALS['umw_outreach_mods_obj'], 'UMW\Outreach\News' );
+			$this->is_news   = is_a( $GLOBALS['umw_outreach_mods_obj'], 'UMW\Outreach\News' );
 
 			/**
 			 * Add extra info to the post list on News & Events sites
@@ -66,6 +67,12 @@ if ( ! class_exists( 'Columns' ) ) {
 			add_action( 'manage_edit-post_sortable_columns', array( $this, 'custom_posts_sortable' ) );
 			add_action( 'manage_edit-umw-localist_sortable_columns', array( $this, 'custom_posts_sortable' ) );
 			add_action( 'pre_get_posts', array( $this, 'do_sortable' ) );
+
+			add_action( 'quick_edit_custom_box', array( $this, 'quick_edit_boxes' ), 10, 2 );
+            add_action( 'bulk_edit_custom_box', array( $this, 'quick_edit_boxes' ), 10, 2 );
+			add_action( 'save_post', array( $this, 'save_quick_edit' ) );
+            add_action( 'save_post', array( $this, 'save_bulk_edit' ) );
+			add_action( 'admin_print_footer_scripts', array( $this, 'admin_footer_scripts' ) );
 		}
 
 
@@ -75,8 +82,8 @@ if ( ! class_exists( 'Columns' ) ) {
 		 * @param array $columns the list of post columns
 		 *
 		 * @access public
-		 * @since  2023.01
 		 * @return array the updated list of columns
+		 * @since  2023.01
 		 */
 		public function posts_columns( array $columns ): array {
 			if ( $this->is_events ) {
@@ -100,8 +107,8 @@ if ( ! class_exists( 'Columns' ) ) {
 		 * @param int $post_id the ID of the post being listed
 		 *
 		 * @access public
-		 * @since  2023.01
 		 * @return void
+		 * @since  2023.01
 		 */
 		public function custom_posts_columns( string $column_name, int $post_id ): void {
 			if ( 'featured' !== $column_name ) {
@@ -134,8 +141,8 @@ if ( ! class_exists( 'Columns' ) ) {
 		 * @param array $columns the list of sortable columns
 		 *
 		 * @access public
-		 * @since  2023.01
 		 * @return array the updated list of columns
+		 * @since  2023.01
 		 */
 		public function custom_posts_sortable( array $columns ): array {
 			if ( ! $this->is_news && ! $this->is_events ) {
@@ -153,15 +160,145 @@ if ( ! class_exists( 'Columns' ) ) {
 		 * @param \WP_Query $query the existing post query
 		 *
 		 * @access public
-		 * @since  2023.01
 		 * @return void
+		 * @since  2023.01
 		 */
 		public function do_sortable( $query ) {
-			$orderby = $query->get('orderby');
+			$orderby = $query->get( 'orderby' );
 			if ( 'featured' === $orderby ) {
-				$query->set('meta_key', 'umw_cb_post_is_featured');
-				$query->set('orderby','meta_value');
+				$query->set( 'meta_key', 'umw_cb_post_is_featured' );
+				$query->set( 'orderby', 'meta_value' );
 			}
+		}
+
+		/**
+		 * Set up the Quick Edit field(s)
+		 *
+		 * @param string $column the name of the column being handled
+		 * @param string $post_type the post type handle
+		 *
+		 * @access public
+		 * @return void
+		 * @since  2023.01
+		 */
+		public function quick_edit_boxes( string $column, string $post_type ) {
+			if ( ! $this->is_events && ! $this->is_news ) {
+				return;
+			}
+
+			if ( 'featured' !== $column ) {
+				return;
+			}
+
+			if ( ( $this->is_events && 'umw-localist' === $post_type ) || ( $this->is_news && 'post' === $post_type ) ) {
+				printf( '<div class="inline-edit-col">
+						<label>
+							<input type="checkbox" name="umw_cb_post_is_featured" value="1"> %1$s
+						</label>
+					</div>', __( 'Featured', 'umw/outreach' ) );
+
+				return;
+			}
+		}
+
+		/**
+		 * Save the changes made during Quick Edit
+		 *
+		 * @param int $post_id
+		 *
+		 * @access public
+		 * @return void
+		 * @since  2023.01
+		 */
+		public function save_quick_edit( int $post_id ) {
+			if ( ! $this->is_events && ! $this->is_news ) {
+				return;
+			}
+
+			$post_type = get_post_type( $post_id );
+
+			if ( ( $this->is_events && 'umw-localist' === $post_type ) || ( $this->is_news && 'post' === $post_type ) ) {
+
+				// check inline edit nonce
+				if ( ! wp_verify_nonce( $_POST['_inline_edit'], 'inlineeditnonce' ) ) {
+					return;
+				}
+
+				// update checkbox
+				$featured = ( isset( $_POST['umw_cb_post_is_featured'] ) && '1' == $_POST['umw_cb_post_is_featured'] ) ? '1' : '0';
+				update_post_meta( $post_id, 'umw_cb_post_is_featured', $featured );
+
+			}
+		}
+
+		/**
+		 * Save the changes made during Bulk Edit
+		 *
+		 * @param int $post_id
+		 *
+		 * @access public
+		 * @return void
+		 * @since  2023.01
+		 */
+		public function save_bulk_edit( int $post_id ) {
+			if ( ! $this->is_events && ! $this->is_news ) {
+				return;
+			}
+
+			$post_type = get_post_type( $post_id );
+
+			if ( ( $this->is_events && 'umw-localist' === $post_type ) || ( $this->is_news && 'post' === $post_type ) ) {
+
+				// check bulk edit nonce
+				if ( ! wp_verify_nonce( $_REQUEST[ '_wpnonce' ], 'bulk-posts' ) ) {
+					return;
+				}
+
+				// update checkbox
+				$featured = ( isset( $_REQUEST['umw_cb_post_is_featured'] ) && '1' == $_REQUEST['umw_cb_post_is_featured'] ) ? '1' : '0';
+				update_post_meta( $post_id, 'umw_cb_post_is_featured', $featured );
+
+			}
+		}
+
+		/**
+		 * Output some javascript for the quick edit box
+		 *
+		 * @access public
+		 * @return void
+		 * @since  2023.01
+		 */
+		public function admin_footer_scripts() {
+			?>
+            <script>
+                jQuery(function ($) {
+
+                    const wp_inline_edit_function = inlineEditPost.edit;
+
+                    // we overwrite the it with our own
+                    inlineEditPost.edit = function (post_id) {
+
+                        // let's merge arguments of the original function
+                        wp_inline_edit_function.apply(this, arguments);
+
+                        // get the post ID from the argument
+                        if (typeof (post_id) == 'object') { // if it is object, get the ID number
+                            post_id = parseInt(this.getId(post_id));
+                        }
+
+                        // add rows to variables
+                        const edit_row = $('#edit-' + post_id)
+                        const post_row = $('#post-' + post_id)
+
+                        const featuredProduct = 'Yes' === $('.column-featured .screen-reader-text', post_row).text();
+
+                        // populate the inputs with column data
+                        $(':input[name="umw_cb_post_is_featured"]', edit_row).prop('checked', featuredProduct);
+
+                    }
+                });
+            </script>
+			<?php
 		}
 	}
 }
