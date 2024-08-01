662ddc4de2327e208c33055d94279de16334dbd3
Curtiss Grymala <cgrymala@umw.edu>
2024-08-01 09:38:39 -0400

    Update bundled version of ACF Pro to 6.3.4

--- lib/acf/acf/acf.php
+++ lib/acf/acf/acf.php
@@ -9,14 +9,14 @@
  * Plugin Name:       Advanced Custom Fields PRO
  * Plugin URI:        https://www.advancedcustomfields.com
  * Description:       Customize WordPress with powerful, professional and intuitive fields.
- * Version:           6.2.6.1
+ * Version:           6.3.4
  * Author:            WP Engine
  * Author URI:        https://wpengine.com/?utm_source=wordpress.org&utm_medium=referral&utm_campaign=plugin_directory&utm_content=advanced_custom_fields
  * Update URI:        https://www.advancedcustomfields.com/pro
  * Text Domain:       acf
  * Domain Path:       /lang
- * Requires PHP:      7.0
- * Requires at least: 5.8
+ * Requires PHP:      7.4
+ * Requires at least: 6.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -36,7 +36,7 @@ if ( ! class_exists( 'ACF' ) ) {
 		 *
 		 * @var string
 		 */
-		public $version = '6.2.6.1';
+		public $version = '6.3.4';
 
 		/**
 		 * The plugin settings array.
@@ -64,8 +64,6 @@ if ( ! class_exists( 'ACF' ) ) {
 		 *
 		 * @date    23/06/12
 		 * @since   5.0.0
-		 *
-		 * @return  void
 		 */
 		public function __construct() {
 			// Do nothing.
@@ -76,8 +74,6 @@ if ( ! class_exists( 'ACF' ) ) {
 		 *
 		 * @date    28/09/13
 		 * @since   5.0.0
-		 *
-		 * @return  void
 		 */
 		public function initialize() {
 
@@ -133,6 +129,7 @@ if ( ! class_exists( 'ACF' ) ) {
 				'preload_blocks'          => true,
 				'enable_shortcode'        => true,
 				'enable_bidirection'      => true,
+				'enable_block_bindings'   => true,
 			);
 
 			// Include utility functions.
@@ -146,6 +143,7 @@ if ( ! class_exists( 'ACF' ) ) {
 			// Include classes.
 			acf_include( 'includes/class-acf-data.php' );
 			acf_include( 'includes/class-acf-internal-post-type.php' );
+			acf_include( 'includes/class-acf-site-health.php' );
 			acf_include( 'includes/fields/class-acf-field.php' );
 			acf_include( 'includes/locations/abstract-acf-legacy-location.php' );
 			acf_include( 'includes/locations/abstract-acf-location.php' );
@@ -166,6 +164,14 @@ if ( ! class_exists( 'ACF' ) ) {
 			acf_include( 'includes/acf-value-functions.php' );
 			acf_include( 'includes/acf-input-functions.php' );
 			acf_include( 'includes/acf-wp-functions.php' );
+
+			// Override the shortcode default value based on the version when installed.
+			$first_activated_version = acf_get_version_when_first_activated();
+
+			// Only enable shortcode by default for versions prior to 6.3
+			if ( $first_activated_version && version_compare( $first_activated_version, '6.3', '>=' ) ) {
+				$this->settings['enable_shortcode'] = false;
+			}
 
 			// Include core.
 			acf_include( 'includes/fields.php' );
@@ -244,8 +250,6 @@ if ( ! class_exists( 'ACF' ) ) {
 		 *
 		 * @date    28/09/13
 		 * @since   5.0.0
-		 *
-		 * @return  void
 		 */
 		public function init() {
 
@@ -310,6 +314,7 @@ if ( ! class_exists( 'ACF' ) ) {
 			acf_include( 'includes/fields/class-acf-field-date_time_picker.php' );
 			acf_include( 'includes/fields/class-acf-field-time_picker.php' );
 			acf_include( 'includes/fields/class-acf-field-color_picker.php' );
+			acf_include( 'includes/fields/class-acf-field-icon_picker.php' );
 			acf_include( 'includes/fields/class-acf-field-message.php' );
 			acf_include( 'includes/fields/class-acf-field-accordion.php' );
 			acf_include( 'includes/fields/class-acf-field-tab.php' );
@@ -386,6 +391,12 @@ if ( ! class_exists( 'ACF' ) ) {
 			 */
 			do_action( 'acf/include_taxonomies', ACF_MAJOR_VERSION );
 
+			// If we're on 6.5 or newer, load block bindings. This will move to an autoloader in 6.3.
+			if ( version_compare( get_bloginfo( 'version' ), '6.5-beta1', '>=' ) ) {
+				acf_include( 'includes/Blocks/Bindings.php' );
+				new ACF\Blocks\Bindings();
+			}
+
 			/**
 			 * Fires after ACF is completely "initialized".
 			 *
@@ -402,8 +413,6 @@ if ( ! class_exists( 'ACF' ) ) {
 		 *
 		 * @date    22/10/2015
 		 * @since   5.3.2
-		 *
-		 * @return  void
 		 */
 		public function register_post_types() {
 			$cap = acf_get_setting( 'capability' );
@@ -482,8 +491,6 @@ if ( ! class_exists( 'ACF' ) ) {
 		 *
 		 * @date    22/10/2015
 		 * @since   5.3.2
-		 *
-		 * @return  void
 		 */
 		public function register_post_status() {
 
@@ -765,7 +772,13 @@ if ( ! class_exists( 'ACF' ) ) {
 				// If acf_version is set, this isn't the first activated version, so leave it unset so it's legacy.
 				if ( null === get_option( 'acf_version', null ) ) {
 					update_option( 'acf_first_activated_version', ACF_VERSION, true );
+
+					do_action( 'acf/first_activated' );
 				}
+			}
+
+			if ( acf_is_pro() ) {
+				do_action( 'acf/activated_pro' );
 			}
 		}
 	}
--- lib/acf/acf/includes/fields/class-acf-field-tab.php
+++ lib/acf/acf/includes/fields/class-acf-field-tab.php
@@ -6,19 +6,16 @@ if ( ! class_exists( 'acf_field_tab' ) ) :
 
 		public $show_in_rest = false;
 
-		/*
-		*  __construct
-		*
-		*  This function will setup the field type data
-		*
-		*  @type    function
-		*  @date    5/03/2014
-		*  @since   5.0.0
-		*
-		*  @param   n/a
-		*  @return  n/a
-		*/
-
+		/**
+		 * This function will setup the field type data
+		 *
+		 * @type    function
+		 * @date    5/03/2014
+		 * @since   5.0.0
+		 *
+		 * @param   n/a
+		 * @return  n/a
+		 */
 		function initialize() {
 
 			// vars
@@ -32,74 +29,72 @@ if ( ! class_exists( 'acf_field_tab' ) ) :
 			$this->defaults      = array(
 				'placement' => 'top',
 				'endpoint'  => 0, // added in 5.2.8
+				'selected'  => 0, // added in 6.3
 			);
 		}
 
-
-		/*
-		*  render_field()
-		*
-		*  Create the HTML interface for your field
-		*
-		*  @param   $field - an array holding all the field's data
-		*
-		*  @type    action
-		*  @since   3.6
-		*  @date    23/01/13
-		*/
-
-		function render_field( $field ) {
-
-			// vars
+		/**
+		 * Output the HTML required for a tab.
+		 *
+		 * @since 3.6
+		 *
+		 * @param array $field An array of the field data.
+		 */
+		public function render_field( $field ) {
 			$atts = array(
 				'href'           => '',
 				'class'          => 'acf-tab-button',
 				'data-placement' => $field['placement'],
 				'data-endpoint'  => $field['endpoint'],
 				'data-key'       => $field['key'],
+				'data-selected'  => $field['selected'],
 			);
 
+			if ( isset( $field['unique_tab_key'] ) && ! empty( $field['unique_tab_key'] ) ) {
+				$atts['data-unique-tab-key'] = $field['unique_tab_key'];
+			}
+
 			if ( isset( $field['settings-type'] ) ) {
-				$atts['class'] .= ' acf-settings-type-' . acf_slugify( $field['settings-type'] );
+				$atts['data-settings-type'] = acf_slugify( $field['settings-type'] );
+				$atts['class']             .= ' acf-settings-type-' . acf_slugify( $field['settings-type'] );
+			}
+
+			if ( isset( $field['class'] ) && ! empty( $field['class'] ) ) {
+				$atts['class'] .= ' ' . $field['class'];
 			}
 
 			?>
-		<a <?php echo acf_esc_attrs( $atts ); ?>><?php echo acf_esc_html( $field['label'] ); ?></a>
+			<a <?php echo acf_esc_attrs( $atts ); ?>><?php echo acf_esc_html( $field['label'] ); ?></a>
 			<?php
 		}
 
-
-
-		/*
-		*  render_field_settings()
-		*
-		*  Create extra options for your field. This is rendered when editing a field.
-		*  The value of $field['name'] can be used (like bellow) to save extra data to the $field
-		*
-		*  @param   $field  - an array holding all the field's data
-		*
-		*  @type    action
-		*  @since   3.6
-		*  @date    23/01/13
-		*/
-
+		/**
+		 * Create extra options for your field. This is rendered when editing a field.
+		 * The value of $field['name'] can be used (like bellow) to save extra data to the $field
+		 *
+		 * @param   $field  - an array holding all the field's data
+		 *
+		 * @type    action
+		 * @since   3.6
+		 * @date    23/01/13
+		 */
 		function render_field_settings( $field ) {
 
 			/*
-			// message
-			$message = '';
-			$message .= '<p>' . __( 'Use "Tab Fields" to better organize your edit screen by grouping fields together.', 'acf') . '</p>';
-			$message .= '<p>' . __( 'All fields following this "tab field" (or until another "tab field" is defined) will be grouped together using this field\'s label as the tab heading.','acf') . '</p>';
+				// message
+				$message = '';
+				$message .= '<p>' . __( 'Use "Tab Fields" to better organize your edit screen by grouping fields together.', 'acf') . '</p>';
+				$message .= '<p>' . __( 'All fields following this "tab field" (or until another "tab field" is defined) will be grouped together using this field\'s label as the tab heading.','acf') . '</p>';
 
 
-			// default_value
-			acf_render_field_setting( $field, array(
-			'label'         => __('Instructions','acf'),
-			'instructions'  => '',
-			'name'          => 'notes',
-			'type'          => 'message',
-			'message'       => $message,
-			));
+				// default_value
+				acf_render_field_setting( $field, array(
+				'label'         => __('Instructions','acf'),
+				'instructions'  => '',
+				'name'          => 'notes',
+				'type'          => 'message',
+				'message'       => $message,
+				));
 			*/
 
 			// preview_size
@@ -130,19 +125,17 @@ if ( ! class_exists( 'acf_field_tab' ) ) :
 		}
 
 
-		/*
-		*  load_field()
-		*
-		*  This filter is appied to the $field after it is loaded from the database
-		*
-		*  @type    filter
-		*  @since   3.6
-		*  @date    23/01/13
-		*
-		*  @param   $field - the field array holding all the field options
-		*
-		*  @return  $field - the field array holding all the field options
-		*/
+		/**
+		 * This filter is appied to the $field after it is loaded from the database
+		 *
+		 * @type    filter
+		 * @since   3.6
+		 * @date    23/01/13
+		 *
+		 * @param   $field - the field array holding all the field options
+		 *
+		 * @return  $field - the field array holding all the field options
+		 */
 		function load_field( $field ) {
 
 			// remove name to avoid caching issue
